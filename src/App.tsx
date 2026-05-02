2bb01c8d7e0e9507bf3ae8db74d3b24f357a8854
David <243669271+DavidSeaX@users.noreply.github.com>
2026-05-02 00:42:40 +0000

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import { Authenticator } from '@aws-amplify/ui-react';
-import ProtectedRoute from './components/ProtectedRoute';
 import Bookshelf from './pages/Bookshelf';
+// add imports for Home, Discover, ProtectedRoute/Pages as needed
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />          {/* public */}
-      <Route path="/discover" element={<Discover />} />
-      <Route path="/*" element={<ProtectedRoute><Pages /></ProtectedRoute>} />  {/* all else behind login */}
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="/discover" element={<div>Discover</div>} />
       <Route path="/bookshelf" element={<Bookshelf />} />
+      {/* other routes before catch-all */}
     </Routes>
   );
 }
+
+export default App;
